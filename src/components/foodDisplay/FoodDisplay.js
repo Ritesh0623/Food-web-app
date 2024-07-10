7ae8b06d7656296f2174b88402e349358fa4df8c
Ritesh0623 <143871042+Ritesh0623@users.noreply.github.com>
2024-07-10 19:16:36 +0530

    Food Web app created

---
+++ src/components/foodDisplay/FoodDisplay.js
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import './FoodDisplay.css'
+import { StoreContext } from '../../context/StoreContext'
+import FoodItems from '../foodItems/FoodItems';
+
+const FoodDisplay = ({category}) => {
+
+        const {food_list} = useContext(StoreContext);
+
+  return (
+    <div className='food-display' id='food-display'>
+        <h2>Top dishes near you</h2>
+        <div className="food-display-list">
+            {food_list.map((item, index) => {
+                if(category==="All" || category===item.category){
+                    return <FoodItems key={index} id={item._id} name={item.name} price={item.price} 
+                    image={item.image} description={item.description} />
+                }
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default FoodDisplay
